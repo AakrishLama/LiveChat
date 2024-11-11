481b245eaba495f338e700ee4e57a621ec27f9b0
Aakrish Lama <al226na@student.lnu.se>
2024-11-11 14:39:43 +0100

    creating interface for chat component

--- frontend/src/Components/Chat.js
+++ frontend/src/Components/Chat.js
@@ -3,8 +3,14 @@ import React from 'react'
 export default function Chat() {
   return (
     <div>
-        chat message.
-      
+      <div className='header'></div>
+      <h1>Welcome to Group </h1>
+
+      <div className='body'></div>
+      <div className='footer'>
+        <input style={{ width: '80%' }} type="text" placeholder='message...' ></input>
+        <button className="btn btn-success mb-3 px-4 mx-auto">Send</button>
+      </div>
     </div>
   )
 }
