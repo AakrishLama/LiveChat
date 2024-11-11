2299897d45ef16b14ee7ab845fb9addb360ff787
Aakrish Lama <al226na@student.lnu.se>
2024-11-11 14:30:27 +0100

    join-room event in backend

--- Backend/index.js
+++ Backend/index.js
@@ -2,23 +2,27 @@ const express = require("express")
 const app = express()
 const http = require("http")
 const cors = require("cors")
-const { Server }= require("socket.io")
+const { Server } = require("socket.io")
 
 app.use(cors())
 
 const server = http.createServer(app)
 
 const io = new Server(server, {
-    cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"],
-    },
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
 });
 
 io.on("connection", (socket) => {
-    console.log(`User Connected: ${socket.id}`)
+  // console.log(`User Connected: ${socket.id}`)
+  socket.on("join-room", (data) => {
+    socket.join(data)
+    console.log(socket.id, "joined room", data)
+  })
 })
 
-server.listen(3001, ()=>{
+server.listen(3001, () => {
   console.log("server running")
 });
