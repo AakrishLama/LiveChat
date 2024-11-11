2088dfcef0a33da4840155b9d8ba31ade1e983f2
Aakrish Lama <al226na@student.lnu.se>
2024-11-11 15:34:33 +0100

    displaying messages dynamically on each side of client

--- frontend/src/Components/Chat.js
+++ frontend/src/Components/Chat.js
@@ -37,10 +37,30 @@ export default function Chat({ socket, user, room }) {
 
       <div className='body'></div>
       {msgList.map((data, index) => {
-        return (<div key={index}>
-          {data.message}
-        </div>)
+        return (
+          <div key={index} className="mb-3">
+            {/* Conditionally render messages from the user or others */}
+            {user === data.author ? (
+              <div className="d-flex justify-content-end">
+                <div className="msg-container w-25 border border-2 border-dark bg-grey p-2">
+                  <p className="author text-success">You</p>
+                  <p className="msg">{data.message}</p>
+                  <p className="time text-end">{data.time}</p>
+                </div>
+              </div>
+            ) : (
+              <div className="d-flex justify-content-start">
+                <div className="msg-container w-25 border border-2 border-dark bg-grey p-2">
+                  <p className="author text-primary">{data.author}</p>
+                  <p className="msg">{data.message}</p>
+                  <p className="time text-end">{data.time}</p>
+                </div>
+              </div>
+            )}
+          </div>
+        );
       })}
+      <hr className="my-3 border border-2 border-dark" style={{ width: '100%' }} />
       <div className='footer'>
         <input style={{ width: '80%' }} type="text" placeholder='message...' value={message} onChange={(e) => setMessage(e.target.value)}></input>
         <button className="btn btn-success mb-3 px-4 mx-auto" onClick={() => sendMessage()}>Send</button>
