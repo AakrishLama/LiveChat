87e04a89b0ddaf14c2200f7abebb7238495bbd0f
Aakrish Lama <al226na@student.lnu.se>
2024-11-11 14:27:37 +0100

    connect client with backend through socket.io

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,39 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import io from "socket.io-client";
+import "./App.css";
+import { useState } from "react";
 
+const socket = io.connect("http://localhost:3001");
 
 function App() {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+
+  const joinRoom = () => {
+    if (name && room) {
+      socket.emit("join-room", room)
+    }
+  }
   return (
     <div className="App">
-     
+      <div className="user-container text-center">
+        <h1 className="mb-5">Chat Web</h1>
+        <input
+          className="form-control mb-3 mx-auto"
+          type="text"
+          placeholder="name"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          className="form-control mb-3 mx-auto"
+          type="text"
+          placeholder="room"
+          onChange={(e) => setRoom(e.target.value)}
+        />
+        <button className="btn btn-primary px-4 mx-auto justify-content-center" onClick={joinRoom}>
+          Join
+        </button>
+      </div>
     </div>
   );
 }
